74d24b7fd67e96415f26340cdcaf78bc69f6245d
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-06-12 17:51:00 -0300

    ensure SignUpController return 500 if AddAccount throws

--- jest.config.ts
+++ jest.config.ts
@@ -10,5 +10,21 @@ export default {
   },
 };
 
+// module.exports = {
+//   roots: ["<rootDir>/src"],
+//   clearMocks: true,
+//   collectCoverage: true,
+//   collectCoverageFrom: [
+//     "<rootDir>/src/presentation/controllers/signup/signup.spec.ts",
+//     "<rootDir>/src/presentation/controllers/signup/signup.ts",
+//   ],
+//   coverageDirectory: "coverage",
+//   coverageProvider: "v8",
+//   transform: {
+//     ".+\\.ts$": "ts-jest",
+//   },
+// };
+
+
 
 
