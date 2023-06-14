0028db8f6a16cf9ddab780a845002a88c60742ea
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-06-14 19:37:19 -0300

    ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -14,16 +14,10 @@ export class DbAddAccount implements AddAccount {
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password);
-    await this.addAccountRepository.add(
+    const account = await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword })
     );
-    const newAccount: AccountModel = {
-      id: "1",
-      name: accountData.name,
-      email: accountData.email,
-      password: accountData.password,
-    };
 
-    return new Promise((resolve) => resolve(newAccount));
+    return account;
   }
 }
