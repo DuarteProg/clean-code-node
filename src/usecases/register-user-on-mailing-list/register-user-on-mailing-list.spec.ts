9d3f1a19487f9ff9644775869ebbd91264a7edfa
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-10-13 00:32:20 -0300

    user repository interface

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,17 +1,17 @@
+import { UserRepository } from "./ports/user-repository";
 import { UserData } from "./user-data";
 
 describe("Register user on mailing list use case", () => {
   test("should add user with complete data to mailing list", async () => {
     const users: UserData[] = [];
-    console.log(users);
-    // const repo: UserRepository = new InMemoryUserRepository(users);
-    // const usecase: RegisterUserOnMainlingList = new RegisterUserOnMainlingList(
-    //   repo
-    // );
-    // const name = "any_name";
-    // const email = "any@email.com";
-    // const response = await usecase.RegisterUserOnMainlingList({ name, email });
-    // const user = repo.findUserByEmail("any@email.com");
-    // expect((await user).name).toBe("any@email.com");
+    const repo: UserRepository = new InMemoryUserRepository(users);
+    const usecase: RegisterUserOnMainlingList = new RegisterUserOnMainlingList(
+      repo
+    );
+    const name = "any_name";
+    const email = "any@email.com";
+    const response = await usecase.RegisterUserOnMainlingList({ name, email });
+    const user = repo.findUserByEmail("any@email.com");
+    expect((await user).name).toBe("any@email.com");
   });
 });
